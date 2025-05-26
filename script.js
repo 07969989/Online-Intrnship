34e78df68b4d4141ff1f70bb96f9702f89c20630
Muhammad Ibrar <162678202+07969989@users.noreply.github.com>
2025-05-25 22:45:53 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Main JavaScript File
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Theme loaded successfully!');
+});
+
+  function donateNow() {
+    alert("Redirecting to donation page...");
+    window.location.href = "https://your-donation-page.com";
+  }
